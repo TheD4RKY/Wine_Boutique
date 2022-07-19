aa17ecb6cfb988b9d38beb59a698aae882714e1d
TheD4RKY <growlbox123g@gmail.com>
2022-07-19 15:07:26 +0200

    work haha

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,4 @@
-function myFunction(x) {
-  x.classList.toggle("change");
-  var y = document.getElementById("translatetoggle");
- y.classList.toggle("active");
- y.classList.toggle("menu_overlay");
-
-/* let overlay = document.querySelector(".menu_overlay");
+let overlay = document.querySelector(".menu_overlay");
 let open = document.querySelector(".fa");
 let close = document.querySelector(".fa-times");
 
@@ -14,5 +8,4 @@ open.addEventListener("click", () => {
 
 close.addEventListener("click", () => {
   overlay.style.transform = "translateY(-110%)";
-});*/
-}
+});
